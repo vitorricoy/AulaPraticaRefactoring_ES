073618ba2219caa7de0e9f5d928a6830d4c623c9
Vitor Rodarte Ricoy <vitorricoy@gmail.com>
2021-08-02 11:38:17 +0000

    Commit 16

--- Customer.java
+++ Customer.java
@@ -18,24 +18,14 @@ public class Customer {
    }
   
   public String statement() {
-      Enumeration rentals = _rentals.elements();
-      String result = "Rental Record for " + getName() + "\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-
-         // show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-                  String.valueOf(each.getCharge()) + "\n";
-      }
-
-      // add footer lines
-      result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-      result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
-                     " frequent renter points";
-      return result;
+      return new TextStatement().value(this);
    }
-      
-   private double getTotalCharge() {
+  
+  public Enumeration getRentals() {
+	   return _rentals.elements();
+   }
+
+   public double getTotalCharge() {
       double result = 0;
       Enumeration rentals = _rentals.elements();
       while (rentals.hasMoreElements()) {
@@ -45,7 +35,7 @@ public class Customer {
          return result;
    }
 
-   private int getTotalFrequentRenterPoints(){
+   public int getTotalFrequentRenterPoints(){
       int result = 0;
       Enumeration rentals = _rentals.elements();
       while (rentals.hasMoreElements()) {
