8177f80a9d65e2e7fad9f6a48ec5f70ecac5d963
Vitor Rodarte Ricoy <vitorricoy@gmail.com>
2021-08-02 11:19:56 +0000

    Commit 5

--- Customer.java
+++ Customer.java
@@ -25,8 +25,6 @@ public class Customer {
      while (rentals.hasMoreElements()) {
         Rental each = (Rental) rentals.nextElement();
 
-        double thisAmount = each.getCharge();
-
         // add frequent renter points
         frequentRenterPoints ++;
         // add bonus for a two day new release rental
@@ -35,8 +33,8 @@ public class Customer {
 
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += thisAmount;
+            String.valueOf(each.getCharge()) + "\n";
+        totalAmount += each.getCharge();
 
      }
      //add footer lines
