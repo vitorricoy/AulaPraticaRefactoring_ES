b9f253eab3b0da3d191c544c8d82e465c664f677
Vitor Rodarte Ricoy <vitorricoy@gmail.com>
2021-08-02 11:29:55 +0000

    Commit 11

--- Movie.java
+++ Movie.java
@@ -5,19 +5,31 @@ public class Movie {
   public static final int  NEW_RELEASE = 1;
 
   private String _title;
-  private int _priceCode;
+  private Price _price;
 
-  public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
-  }
-
-  public int getPriceCode() {
-      return _priceCode;
-  }
-
+   public Movie(String name, int priceCode) {
+      _title = name;
+      setPriceCode(priceCode);
+   }
+    
+   public int getPriceCode() {
+      return _price.getPriceCode();
+   }
+   
   public void setPriceCode(int arg) {
-      _priceCode = arg;
+      switch (arg) {
+         case REGULAR:
+            _price = new RegularPrice();
+            break;
+         case CHILDRENS:
+            _price = new ChildrensPrice();
+            break;
+         case NEW_RELEASE:
+            _price = new NewReleasePrice();
+            break;
+         default:
+            throw new IllegalArgumentException("Incorrect Price Code");
+      }
   }
 
   public String getTitle (){
@@ -27,7 +39,7 @@ public class Movie {
   public double getCharge(int daysRented){ 
       double thisAmount = 0;
       //determine amounts for rental
-      switch (this._priceCode) {
+      switch (this.getPriceCode()) {
           case Movie.REGULAR:
             thisAmount += 2;
             if (daysRented > 2)
@@ -50,7 +62,7 @@ public class Movie {
       // add frequent renter points
       frequentRenterPoints ++;
       // add bonus for a two day new release rental
-      if ((this._priceCode == Movie.NEW_RELEASE) &&
+      if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
           daysRented > 1) frequentRenterPoints ++;
       return frequentRenterPoints;
    }
